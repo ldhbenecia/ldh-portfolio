873139344967ffc0cfcdfc84890bc7a767e870c5
DongHyeok Lim <jh07050@gmail.com>
2023-04-16 20:01:38 +0900

    initial commit

---
+++ components/layout.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from "react";
+import Header from "./Header/header"
+import Footer from "./Footer/footer";
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div className="bg-primary">
+      <div>{children}</div>
+    </div>
+  );
+}

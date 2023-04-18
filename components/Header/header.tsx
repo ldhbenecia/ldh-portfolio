f9dbc10ce7296184fe79516e722fe9e437908ecc
DongHyeok Lim <jh07050@gmail.com>
2023-04-18 17:14:43 +0900

    Style: Header fixed setting

--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -14,7 +14,7 @@ const Header: React.FC<HeaderProps> = ({ mainRef, introRef, projectsRef }) => {
   };
 
   return (
-    <header className="text-gray-600 body-font">
+    <header className="fixed w-full z-10 dark:bg-slate-800 text-gray-600 body-font">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
           <svg
